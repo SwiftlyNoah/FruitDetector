465aa8519db2f5994a71e585700142e653c7e19e
SwiftlyNoah <devnoahapps@gmail.com>
2023-09-30 16:07:00 -0400

    Initial Commit

---
+++ FruitDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FruitDetector
+//
+//  Created by Noah Brauner on 9/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruitDetector/FruitDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitDetectorApp.swift
+//  FruitDetector
+//
+//  Created by Noah Brauner on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
